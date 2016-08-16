00a8a36ca4e31fb8b641d7928de49f8531ed9769
Dmitriy Volkov <wldhx@wldhx.me>
2016-08-16 18:25:23 +0300

    feat(ble/characteristic): Implement subscription; impl .value
    Make .value a property with proper setter:
    	- Allow intuitively changing value at runtime (as opposed to
    	  only at init)
    	- Automatically triggers updateValueCallback on every .value update

--- characteristic.js
+++ characteristic.js
@@ -4,21 +4,41 @@ var bleno = require('bleno');
 
 var BlenoCharacteristic = bleno.Characteristic;
 
-var read_count = 0;
-
 var ProstheticFlexStatusCharacteristic = new BlenoCharacteristic({
     uuid: '58d3c1f4-b253-4055-9d02-3932126539f8',
     properties: ['read', 'notify'],
-    value: null,
     onReadRequest:
 	    function(offset, callback) {
 		    if (offset) {
 			    callback(this.RESULT_ATTR_NOT_LONG, null);
 		    }
 
-		    callback(this.RESULT_SUCCESS, Buffer.from([read_count]));
-		    read_count += 1;
-	    }
+		    callback(this.RESULT_SUCCESS, this._value);
+	    },
+    onSubscribe:
+	    function(maxValueSize, updateValueCallback) {
+		    this._updateValueCallback = updateValueCallback;
+	    },
 });
+
+Object.defineProperty(ProstheticFlexStatusCharacteristic, 'value', {
+	get: function() { return this._value; },
+	set: function(value) {
+		this._value = Buffer.from(value);
+
+		if (this._updateValueCallback) {
+			this._updateValueCallback(this._value);
+		}
+	}
+});
+
+var i = 0;
+setInterval(
+	function() {
+		ProstheticFlexStatusCharacteristic.value = [i];
+		i += 1;
+	},
+	1000
+);
 
 module.exports = ProstheticFlexStatusCharacteristic;

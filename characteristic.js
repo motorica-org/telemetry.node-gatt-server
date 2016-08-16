f07ef52f5c6a5d044a396e2637489b52fc8497bf
Dmitriy Volkov <wldhx@wldhx.me>
2016-08-16 16:17:15 +0300

    feat(ble/characteristic): Implement dynamic value
    Changes on every read.

--- characteristic.js
+++ characteristic.js
@@ -4,10 +4,19 @@ var bleno = require('bleno');
 
 var BlenoCharacteristic = bleno.Characteristic;
 
+var read_count = 0;
+
 var ProstheticFlexStatusCharacteristic = new BlenoCharacteristic({
     uuid: '58d3c1f4-b253-4055-9d02-3932126539f8',
     properties: ['read', 'notify'],
-    value: Buffer.from([0])
+    value: null,
+    onReadRequest:
+	    function(offset, callback) {
+		    // FIXME: handle offset
+
+		    callback(this.RESULT_SUCCESS, Buffer.from([read_count]));
+		    read_count += 1;
+	    }
 });
 
 module.exports = ProstheticFlexStatusCharacteristic;

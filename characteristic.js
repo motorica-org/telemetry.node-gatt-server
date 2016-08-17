c3ebe5ce19e1878ff1a1f2de2db24c4d60faf376
Dmitriy Volkov <wldhx@wldhx.me>
2016-08-17 19:45:05 +0300

    refactor(ble/characteristic): Massively prettify code with ES6 features

--- characteristic.js
+++ characteristic.js
@@ -1,31 +1,27 @@
 let bleno = require('bleno');
 
-let ProstheticFlexStatusCharacteristic = new bleno.Characteristic({
-    uuid: '58d3c1f4-b253-4055-9d02-3932126539f8',
-    properties: ['read', 'notify'],
-    onReadRequest:
-	    (offset, callback) => {
-		    if (offset) {
-			    callback(this.RESULT_ATTR_NOT_LONG, null);
-		    }
 
-		    callback(this.RESULT_SUCCESS, this._value);
-	    },
-    onSubscribe:
-	    (maxValueSize, updateValueCallback) => {
-		    this._updateValueCallback = updateValueCallback;
-	    },
-});
+class ProstheticFlexStatusCharacteristic extends bleno.Characteristic {
+	constructor(value) {
+		super({
+			uuid: '58d3c1f4-b253-4055-9d02-3932126539f8',
+			properties: ['read', 'notify'],
+		})
+		this._value = Buffer.from(value);
+	}
+	// we use custom .value attribute because bleno does not care about its' changes by default
+	get value() {
+		return this._value;
+	}
+	set value(value) {
+		if (value === null) { return; }
 
-Object.defineProperty(ProstheticFlexStatusCharacteristic, 'value', {
-	get: () => { return this._value; },
-	set: (value) => {
 		this._value = Buffer.from(value);
 
-		if (this._updateValueCallback) {
-			this._updateValueCallback(this._value);
+		if (this.updateValueCallback) {
+			this.updateValueCallback(this.value);
 		}
 	}
-});
+}
 
-module.exports = ProstheticFlexStatusCharacteristic;
+module.exports = new ProstheticFlexStatusCharacteristic([0]);

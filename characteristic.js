087feb1d66518c0c25af3129607618d6458b9fc9
Dmitriy Volkov <wldhx@wldhx.me>
2016-08-17 19:45:31 +0300

    docs(ble/characteristic): Note that reading is broken

--- characteristic.js
+++ characteristic.js
@@ -1,6 +1,9 @@
 let bleno = require('bleno');
 
 
+// FIXME: reading is currently broken
+// For reasons unknown to me, a mere mortal, :method: onReadRequest seemingly does not get triggered if :attribute: value is impld with getters and setters and ES6 class syntax is used (:commit: 8c54783 is fine).
+// As subscription works fine and we don't actually need reading ATM, I'll leave that be, but that's *weird*.
 class ProstheticFlexStatusCharacteristic extends bleno.Characteristic {
 	constructor(value) {
 		super({

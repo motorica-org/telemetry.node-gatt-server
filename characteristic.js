5b32391a509b7cd38beaa9e243c9e6b2635ec7a8
Dmitriy Volkov <wldhx@wldhx.me>
2016-08-23 11:57:07 +0300

    refactor(*): Reformat * in accordance with ea52a2f

--- characteristic.js
+++ characteristic.js
@@ -1,30 +1,29 @@
 const bleno = require('bleno');
 
-
 // FIXME: reading is currently broken
 // For reasons unknown to me, a mere mortal, :method: onReadRequest seemingly does not get triggered if :attribute: value is impld with getters and setters and ES6 class syntax is used (:commit: 8c54783 is fine).
 // As subscription works fine and we don't actually need reading ATM, I'll leave that be, but that's *weird*.
 class ProstheticFlexStatusCharacteristic extends bleno.Characteristic {
-	constructor(value) {
-		super({
-			uuid: '58d3c1f4-b253-4055-9d02-3932126539f8',
-			properties: ['read', 'notify'],
-		})
-		this._value = Buffer.from(value);
-	}
-	// we use custom .value attribute because bleno does not care about its' changes by default
-	get value() {
-		return this._value;
-	}
-	set value(value) {
-		if (value === null) { return; }
+  constructor(value) {
+    super({
+      uuid: '58d3c1f4-b253-4055-9d02-3932126539f8',
+      properties: ['read', 'notify'],
+    });
+    this._value = Buffer.from(value);
+  }
+  // we use custom .value attribute because bleno does not care about its' changes by default
+  get value() { return this._value; }
+  set value(value) {
+    if (value === null) {
+      return;
+    }
 
-		this._value = Buffer.from(value);
+    this._value = Buffer.from(value);
 
-		if (this.updateValueCallback) {
-			this.updateValueCallback(this.value);
-		}
-	}
+    if (this.updateValueCallback) {
+      this.updateValueCallback(this.value);
+    }
+  }
 }
 
 module.exports = ProstheticFlexStatusCharacteristic;
--- main.js
+++ main.js
@@ -1,19 +1,12 @@
 'use strict';
 
-
 const bleno = require('bleno');
 const ProstheticFlexStatusCharacteristic = require('./characteristic');
 
-
 const mProstheticFlexStatusCharacteristic = new ProstheticFlexStatusCharacteristic([0]);
 
-const ProstheticFlexStatusService = new bleno.PrimaryService({
-	uuid: 'e35c8bac-a062-4e3f-856d-2cfa87f2f171',
-	characteristics: [
-		mProstheticFlexStatusCharacteristic
-	]
-});
-
+const ProstheticFlexStatusService = new bleno.PrimaryService({uuid: 'e35c8bac-a062-4e3f-856d-2cfa87f2f171',
+                                                              characteristics: [mProstheticFlexStatusCharacteristic]});
 
 bleno.on('stateChange', (state) => {
   console.log('on -> stateChange: ' + state);
@@ -30,25 +23,23 @@ bleno.on('advertisingStart', (error) => {
 
   if (!error) {
     bleno.setServices([
-		    ProstheticFlexStatusService,
+      ProstheticFlexStatusService,
     ]);
   }
 });
 
-
 console.log('bleno - echo');
 
-process.stdin.setRawMode(true); // don't require Enter to get keystrokes
-process.stdin.resume(); // resume parent processes's stdin
+process.stdin.setRawMode(true);  // don't require Enter to get keystrokes
+process.stdin.resume();          // resume parent processes's stdin
 process.stdin.setEncoding('utf8');
 console.log('Press any key to update ProstheticFlexStatusCharacteristic\'s value');
 
 let i = 0;
-process.stdin.on('data',
-		(key) => {
-			if (key === '\u0003') { // C-c
-				process.exit();
-			}
-			mProstheticFlexStatusCharacteristic.value = [i];
-			i += 1;
-		});
+process.stdin.on('data', (key) => {
+  if (key === '\u0003') { // C-c
+    process.exit();
+  }
+  mProstheticFlexStatusCharacteristic.value = [i];
+  i += 1;
+});
